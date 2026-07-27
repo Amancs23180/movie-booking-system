e13af491eb47a551d1274bd5307610db0ed96e7f
Amancs23180 <aman.cse23@sbjit.edu.in>
2026-07-27 18:53:33 +0530

    fix: update server path to point to public/data/data.json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const DATA_FILE = path.join(__dirname, 'data.json');
+const DATA_FILE = path.join(__dirname, 'public', 'data', 'data.json');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
